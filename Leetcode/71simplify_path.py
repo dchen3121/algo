7e73b6130335629fd4ebff3ea163e9a15211b041
dchen3121 <dchen001213@gmail.com>
2019-10-31 21:31:44 -0400

    data structure, stacks

--- Leetcode/71simplify_path.py
+++ Leetcode/71simplify_path.py
@@ -3,8 +3,9 @@ class Solution:
     def simplifyPath(self, path: str) -> str:
         stack = []
         for item in path.split('/'):
-            if item == '..' and stack:
-                stack.pop()
+            if item == '..':
+                if stack: 
+                    stack.pop()
             elif item != '' and item != '/':
                 stack.append(item)
         return f'/{"/".join(stack)}'
